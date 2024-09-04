d4f8b463b5253e311c713fe9cc48479f4eb8a6a5
Mantonio02 <mathiasantonio02@gmail.com>
2024-09-04 11:17:19 +0200

    Added domain objects
    Implemented classes Poll, User, Vote, VoteOption and PollManager.

---
+++ src/main/java/MyGroup/MyProject/PollManager.java
@@ -0,0 +1,19 @@
+package MyGroup.MyProject;
+
+import java.util.HashMap;
+
+public class PollManager {
+    private HashMap<User, Poll> userPoll;
+
+    public PollManager() {
+
+    }
+
+    public HashMap<User, Poll> getUserPoll() {
+        return userPoll;
+    }
+
+    public void setUserPoll(HashMap<User, Poll> userPoll) {
+        this.userPoll = userPoll;
+    }
+}
