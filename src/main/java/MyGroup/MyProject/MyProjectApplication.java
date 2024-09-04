e0ce4e1ecb8df5eb1bdf901f87d16385c09cff22
Mantonio02 <65313931+Mantonio02@users.noreply.github.com>
2024-09-04 10:18:33 +0200

    Update MyProjectApplication.java

--- src/main/java/MyGroup/MyProject/MyProjectApplication.java
+++ src/main/java/MyGroup/MyProject/MyProjectApplication.java
@@ -2,12 +2,19 @@ package MyGroup.MyProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyProjectApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
