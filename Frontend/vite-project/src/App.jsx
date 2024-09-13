88a5f3b4102d5bd8c994a6e532ee471e237c5a7f
Mantonio02 <mathiasantonio02@gmail.com>
2024-09-13 22:10:50 +0200

    Implemented presets for compulsory components.

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,1 +1,23 @@
 import './App.css'
+
+function CreateUserComponent() {
+    return <p>This component creates a user.</p>;
+}
+
+function CreatePollComponent() {
+    return <p>This component creates a poll.</p>;
+}
+
+function VoteComponent() {
+   return <p>This component consists of voting.</p>;
+}
+
+export default function App() {
+    return (
+        <div>
+            <CreateUserComponent />
+            <CreatePollComponent />
+            <VoteComponent />
+        </div>
+    );
+}
