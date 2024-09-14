479f72726c723dd204e27dd3d9492db60c7586f6
Mantonio02 <mathiasantonio02@gmail.com>
2024-09-14 13:52:48 +0200

    Added TODOs.
    The comments are meant as temporary plans/ideas for how to implement the components.

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,15 +1,29 @@
 import './App.css'
 
 function CreateUserComponent() {
-    return <p>This component creates a user.</p>;
+    return (
+        <h1>Create User</h1>
+        // TODO: Implement a form that requires
+        //  username, email and password to be registered.
+    );
 }
 
 function CreatePollComponent() {
-    return <p>This component creates a poll.</p>;
+    return (
+        <h1>Create Poll</h1>
+        // TODO: Implement a form that requires a question and VoteOptions.
+        //  The poll should be registered as released at exact time and date
+        //  as it was published, and the expiration date is up for choice.
+    );
 }
 
 function VoteComponent() {
-   return <p>This component consists of voting.</p>;
+   return (
+       <h1>Vote</h1>
+       // TODO: Implement buttons for VoteOptions.
+       //  Current plan is to iterate through a list of
+       //  them to create a button for each VoteOption (element).
+   );
 }
 
 export default function App() {
